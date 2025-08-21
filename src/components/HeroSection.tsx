48aa8bd02ab2d3b1baccb58b32592f9c3a4acd4d
xryptt <prakharsharma4218@gmail.com>
2025-08-21 19:40:12 +0530

    responsiveness hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -88,7 +88,7 @@ const HeroSection = () => {
       </div>
 
       <div className="container mx-auto px-6 pt-24 pb-12 relative z-10">
-        <div className="text-center" style={{ marginLeft: "6vw" }}>
+        <div className="text-center ml-[7vw] md:ml-[5vw]">
             {/* Large Brand Typography */}
             <div className="mb-8">
               <div className="text-6xl md:text-8xl lg:text-9xl font-black text-white leading-none tracking-tight mb-4 relative">
